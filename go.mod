c7780481e556a0178313436e6e18f26ba4eaac55
Park Min Hyeok <pmhstudio.pmh@gmail.com>
2021-09-06 12:15:43 +0900

    feat: add some things
    Signed-off-by: Park Min Hyeok <pmhstudio.pmh@gmail.com>

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/pmh-only/hcsbutgeckodriver
+
+go 1.17
+
+require (
+	github.com/blang/semver v3.5.1+incompatible // indirect
+	github.com/tebeka/selenium v0.9.9 // indirect
+)
---
+++ main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"log"
+	"os"
+
+	"github.com/tebeka/selenium"
+)
+
+func main() {
+	const (
+		// These paths will be different on your system.
+		seleniumPath    = "bin/selenium-server.jar"
+		geckoDriverPath = "bin/geckodriver.exe"
+		port            = 8080
+	)
+
+	name := os.Args[1]
+	birth := os.Args[2]
+	passwd := os.Args[3]
+
+	opts := []selenium.ServiceOption{
+		selenium.GeckoDriver(geckoDriverPath), // Specify the path to GeckoDriver in order to use Firefox
+	}
+	// selenium.SetDebug(true)
+	service, err := selenium.NewSeleniumService(seleniumPath, port, opts...)
+	if err != nil {
+		panic(err) // panic is used only as an example and is not otherwise recommended.
+	}
+	defer service.Stop()
+
+	// Connect to the WebDriver instance running locally.
+	caps := selenium.Capabilities{"browserName": "firefox"}
+	wd, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d/wd/hub", port))
+	if err != nil {
+		panic(err)
+	}
+	defer wd.Quit()
+
+	if err := wd.Get("https://hcs.eduro.go.kr/"); err != nil {
+		panic(err)
+	}
+
+	click(wd, "#btnConfirm2")
+	click(wd, ".searchBtn")
+	click(wd, "#sidolabel")
+	click(wd, "#sidolabel>option[value=\"16\"]")
+	click(wd, "#crseScCode")
+	click(wd, "#crseScCode>option[value=\"4\"]")
+	typo(wd, "#orgname", "경북소프트웨어고")
+	click(wd, ".searchBtn")
+	click(wd, ".layerSchoolArea>li")
+	click(wd, ".layerFullBtn")
+	typo(wd, "#user_name_input", name)
+	typo(wd, "#birthday_input", birth)
+
+	time.Sleep(1 * time.Second)
+
+	click(wd, "#btnConfirm")
+	click(wd, "#btnConfirm")
+
+	time.Sleep(1 * time.Second)
+
+	click(wd, "#password")
+
+	time.Sleep(1 * time.Second)
+
+	for _, c := range passwd {
+		click(wd, fmt.Sprintf("a[aria-label=\"%c\"]", c))
+	}
+
+	time.Sleep(1 * time.Second)
+
+	click(wd, "#btnConfirm")
+	click(wd, "#btnConfirm")
+
+	time.Sleep(1 * time.Second)
+
+	click(wd, ".name")
+
+	time.Sleep(1 * time.Second)
+
+	click(wd, "#survey_q1a1")
+	click(wd, "#survey_q2a1")
+	click(wd, "#survey_q3a1")
+
+	time.Sleep(1 * time.Second)
+
+	click(wd, "#btnConfirm")
+	click(wd, "#btnConfirm")
+}
+
+func click (wd selenium.WebDriver, selector string) {
+	elem, err := wd.FindElement(selenium.ByCSSSelector, selector)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	elem.Click()
+}
+
+func typo (wd selenium.WebDriver, selector, text string) {
+	elem, err := wd.FindElement(selenium.ByCSSSelector, selector)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	elem.SendKeys(text)
+}
